edb6eea5a681259edb43a563eb8973776fdf77d4
michelsonron7 <michelsonroncete@gmail.com>
2023-04-10 20:02:33 -0300

    Project created

---
+++ src/main/java/br/com/michelsonroncete/course/spring/boot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.michelsonroncete.course.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootApplication.class, args);
+	}
+
+}
