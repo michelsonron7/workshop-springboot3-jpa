9f78d9e2830e910888dc861a40ef08f261425dc4
michelsonron7 <michelsonroncete@gmail.com>
2023-04-11 20:59:08 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/br/com/michelsonroncete/course/spring/boot/entities/User.java
+++ src/main/java/br/com/michelsonroncete/course/spring/boot/entities/User.java
@@ -1,19 +1,21 @@
 package br.com.michelsonroncete.course.spring.boot.entities;
-
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,6 +23,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -74,6 +80,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,5 +101,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
