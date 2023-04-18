49942939f1444c5fda6bcb4704f3913065c0cdca
michelsonron7 <michelsonroncete@gmail.com>
2023-04-17 23:23:29 -0300

    Exception handling - findById

---
+++ src/main/java/br/com/michelsonroncete/course/spring/boot/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.com.michelsonroncete.course.spring.boot.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.michelsonroncete.course.spring.boot.services.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e , HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		var err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/br/com/michelsonroncete/course/spring/boot/services/UserService.java
+++ src/main/java/br/com/michelsonroncete/course/spring/boot/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.michelsonroncete.course.spring.boot.entities.User;
 import br.com.michelsonroncete.course.spring.boot.repositories.UserRepository;
+import br.com.michelsonroncete.course.spring.boot.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
